948f31b5f362b4c14ad139ff36fc3b87570dc4c1
Aydex <adrian.hundseth@hotmail.com>
2015-02-25 11:11:21 +0100

    added some javadoc

--- src/main/java/no/ntnu/stud/security/Authentication.java
+++ src/main/java/no/ntnu/stud/security/Authentication.java
@@ -10,16 +10,41 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
- * Created by adrianh on 23.02.15.
+ * Class responsible for logging in, logging out, and authenticating the user.
+ * For login, an <code>Authentication</code> object has to be created.
+ * The rest of the function are static.
+ *
+ * @author Adrian Hundseth
  */
 public class Authentication {
 
+    /**
+     * The user that is currently authenticated. Is null if no user is logged in.
+     */
     private User loggedInUser = null;
 
+    /**
+     * Returns the logged in user.
+     * @return A <code>User</code> object containing the logged in user.
+     */
     public User getLoggedInUser() {
         return loggedInUser;
     }
 
+    /**
+     * A simple function to see if a user is logged in.
+     * @return Returns true if a user is currently logged in.
+     */
+    public boolean isLoggedIn() {
+        return getLoggedInUser() != null;
+    }
+
+    /**
+     * Authenticates the user with the provided email and password.
+     * @param email <code>String</code> containing the users email.
+     * @param password <code>String</code> containing the users password.
+     * @return Returns true if authentication was successful, false otherwise
+     */
     public static boolean authenticate(String email, String password) {
         Connection con = DBConnector.getCon();
         byte[] hash = null;
@@ -43,6 +68,13 @@ public class Authentication {
         return hash != null && salt != null && SHAHashGenerator.isValid(password.toCharArray(), salt, hash);
     }
 
+    /**
+     * Sends the email and password to the <code>authenticate</code> function,
+     * then returns the logged in user if successful.
+     * @param email The users email
+     * @param password The users password
+     * @return A <code>User</code> object of the currently logged in user. Returns null if authentication failed.
+     */
     public User login(String email, String password) {
         loggedInUser = null;
 
@@ -56,6 +88,9 @@ public class Authentication {
         return loggedInUser;
     }
 
+    /**
+     * Logs out the currently logged in user by setting the <code>loggedInUser</code> object to null.
+     */
     public void logout() {
         loggedInUser = null;
     }
