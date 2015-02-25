bb7699c578df7879e3f183f21dd6828a502132a1
Kristian Elset Boe <kristian.e.boe@gmail.com>
2015-02-25 11:26:02 +0100

    La til forskjellige controllere samt endret App til MainApp i tråd med det jeg mener er best practice

---
+++ src/main/java/no/ntnu/stud/view/calendarViewController.java
@@ -0,0 +1,8 @@
+package no.ntnu.stud.view;
+
+/**
+ * Created by Kristian on 25/02/15.
+ */
+public class CalendarViewController {
+    //dele opp i dayViewController og Week-og/eller monthViewController?
+}
---
+++ src/main/java/no/ntnu/stud/view/rootLayoutController.java
@@ -0,0 +1,29 @@
+package no.ntnu.stud.view;
+
+import javafx.fxml.FXML;
+import no.ntnu.stud.MainApp;
+import org.controlsfx.dialog.Dialogs;
+
+
+/**
+ * Created by Kristian on 25/02/15.
+ */
+public class RootLayoutController {
+    private MainApp mainApp;
+
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
+
+
+    @FXML
+    private void handleAbout() {
+        Dialogs.create().title("CalendarApp").masthead("About").message("Author: Swag\nWebsite: https://github.com/sklirg/fellesprosjekt-2015").showInformation();
+    }
+
+    @FXML
+    private void hanldeExit() {
+        System.exit(0);
+    }
+
+}
