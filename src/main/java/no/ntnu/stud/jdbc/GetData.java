1f51c050c3a31f9e327e3cadc5ae41806b2287a3
Aydex <adrian.hundseth@hotmail.com>
2015-02-25 08:44:17 +0100

    Moved getSmallesRoom from class Reservation to getData. Remember that DBConnector and TimeConverter are classes with static functions, and as such do not need to be initialized before use

--- src/main/java/no/ntnu/stud/jdbc/GetData.java
+++ src/main/java/no/ntnu/stud/jdbc/GetData.java
@@ -1,13 +1,11 @@
 package no.ntnu.stud.jdbc;
 
-import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import no.ntnu.stud.model.Room;
 import no.ntnu.stud.model.User;
 import no.ntnu.stud.util.TimeConverter;
 
-import javax.xml.transform.Result;
 import java.sql.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -105,30 +103,60 @@ public class GetData {
         Timestamp from_time = TimeConverter.localDateTimeToTimestamp(from);
         Timestamp to_time = TimeConverter.localDateTimeToTimestamp(to);
 
-        if (con != null) try {
-            Statement stmt = con.createStatement();
-            String strSelect = "SELECT * FROM room " +
-                    "NATURAL JOIN appointment " +
-                    "WHERE room.roomID='roomID' " +
-                    "AND (('fromDateTime' > " + from_time +
-                    "AND 'fromDateTime' < " + to_time +
-                    "OR ('toDateTime'> " + from_time +
-                    "AND 'toDateTime'< " + to_time +
-                    "OR ('fromDateTime' < " + from_time +
-                    "AND 'toDateTime' > " + to_time + ");";
-            System.out.println("Performing SQL Query [" + strSelect + "]");
-            ResultSet rset = stmt.executeQuery(strSelect);
+        if (con != null) {
+            try {
+                Statement stmt = con.createStatement();
+                String strSelect = "SELECT * FROM room " +
+                        "NATURAL JOIN appointment " +
+                        "WHERE room.roomID='roomID' " +
+                        "AND (('fromDateTime' > " + from_time +
+                        "AND 'fromDateTime' < " + to_time +
+                        "OR ('toDateTime'> " + from_time +
+                        "AND 'toDateTime'< " + to_time +
+                        "OR ('fromDateTime' < " + from_time +
+                        "AND 'toDateTime' > " + to_time + ");";
+                System.out.println("Performing SQL Query [" + strSelect + "]");
+                ResultSet rset = stmt.executeQuery(strSelect);
 
-            while (rset.next()) {
-                int returnRoomID = rset.getInt("roomID");
-                String name = rset.getString("name");
-                int capacity = rset.getInt("capacity");
-                room = new Room(returnRoomID, name, capacity);
+                while (rset.next()) {
+                    int returnRoomID = rset.getInt("roomID");
+                    String name = rset.getString("name");
+                    int capacity = rset.getInt("capacity");
+                    room = new Room(returnRoomID, name, capacity);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } else {
+            System.err.print("No Connection");
         }
+        return room;
+    }
 
+    /**Gets the smallest available room
+     *
+     * @return roomID
+     */
+    public Room getSmallestRoom(LocalDateTime startTime, LocalDateTime endTime, int numPeople){
+        Room room = null;
+        Connection con = DBConnector.getCon();
+        Timestamp startTimestamp = TimeConverter.localDateTimeToTimestamp(startTime);
+        Timestamp endTimestamp = TimeConverter.localDateTimeToTimestamp(endTime);
+        if(con != null){
+            try{
+                Statement stmt = con.createStatement();
+                String sql = "SELECT * FROM room WHERE capacity >="+numPeople+" AND roomID NOT IN( SELECT roomID FROM appointment WHERE ( ("+startTimestamp+" > from_time AND "+startTimestamp+" < to_time) OR ("+endTimestamp+" > from_time AND "+endTimestamp+" <to_time) OR ("+startTimestamp+" < from_time AND "+endTimestamp+" > to_time) ) ) ORDER BY capacity ASC LIMIT 1;";
+                ResultSet rs = stmt.executeQuery(sql);
+                int roomID = rs.getInt("roomID");
+                String roomName = rs.getString("name");
+                int roomCapacity = rs.getInt("capacity");
+                room = new Room(roomID, roomName, roomCapacity);
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.err.print("No Connection");
+        }
         return room;
     }
 }
