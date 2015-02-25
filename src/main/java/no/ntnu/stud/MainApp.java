7345ce4ea9fe3043f7bae458bb8adbdb1e148d93
Kristian Elset Boe <kristian.e.boe@gmail.com>
2015-02-25 10:51:32 +0100

    Satt opp et par kontrollere og gjort litt fx magic i mainApp

---
+++ src/main/java/no/ntnu/stud/MainApp.java
@@ -0,0 +1,57 @@
+package no.ntnu.stud;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import no.ntnu.stud.view.RootLayoutController;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ */
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("CalendarApp");
+
+        initRootLayout();
+
+        //showCalendarOverview();
+
+    }
+
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+
+            RootLayoutController controller = loader.getController();
+            controller.setMainApp(this);
+
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
