46567541068abed7c206f940c60cb1f1d32f1020
kristandre <kristoffer.r.andresen@gmail.com>
2015-02-25 09:53:16 +0100

    Fixed sql in getRoomStatus

--- src/main/java/no/ntnu/stud/jdbc/GetData.java
+++ src/main/java/no/ntnu/stud/jdbc/GetData.java
@@ -108,13 +108,13 @@ public class GetData {
                 Statement stmt = con.createStatement();
                 String strSelect = "SELECT * FROM room " +
                         "NATURAL JOIN appointment " +
-                        "WHERE room.roomID='roomID' " +
-                        "AND (('fromDateTime' > " + from_time +
-                        "AND 'fromDateTime' < " + to_time +
-                        "OR ('toDateTime'> " + from_time +
-                        "AND 'toDateTime'< " + to_time +
-                        "OR ('fromDateTime' < " + from_time +
-                        "AND 'toDateTime' > " + to_time + ");";
+                        "WHERE room.roomID="+roomID+" " +
+                        "AND (("+from_time+" > from_time "+
+                        "AND "+from_time+" <  to_time) " +
+                        "OR ("+to_time+"> from_time "+
+                        "AND "+to_time+"< to_time) "+
+                        "OR ("+from_time+" <  "+
+                        "AND "+to_time+" >  to_time ));";
                 System.out.println("Performing SQL Query [" + strSelect + "]");
                 ResultSet rset = stmt.executeQuery(strSelect);
 
@@ -145,7 +145,17 @@ public class GetData {
         if(con != null){
             try{
                 Statement stmt = con.createStatement();
-                String sql = "SELECT * FROM room WHERE capacity >="+numPeople+" AND roomID NOT IN( SELECT roomID FROM appointment WHERE ( ("+startTimestamp+" > from_time AND "+startTimestamp+" < to_time) OR ("+endTimestamp+" > from_time AND "+endTimestamp+" <to_time) OR ("+startTimestamp+" < from_time AND "+endTimestamp+" > to_time) ) ) ORDER BY capacity ASC LIMIT 1;";
+                String sql = "SELECT * FROM room " +
+                        "WHERE capacity >="+numPeople+" " +
+                        "AND roomID NOT IN( " +
+                        "SELECT roomID FROM appointment " +
+                        "WHERE ( ("+startTimestamp+" > from_time " +
+                        "AND "+startTimestamp+" < to_time) " +
+                        "OR ("+endTimestamp+" > from_time " +
+                        "AND "+endTimestamp+" <to_time) " +
+                        "OR ("+startTimestamp+" < from_time " +
+                        "AND "+endTimestamp+" > to_time) ) ) " +
+                        "ORDER BY capacity ASC LIMIT 1;";
                 ResultSet rs = stmt.executeQuery(sql);
                 int roomID = rs.getInt("roomID");
                 String roomName = rs.getString("name");
@@ -159,4 +169,6 @@ public class GetData {
         }
         return room;
     }
+
+
 }
