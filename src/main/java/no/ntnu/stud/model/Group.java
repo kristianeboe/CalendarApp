ff02e673452ce7e03add82581c7c1da60ee1ee3f
kristandre <kra1992@hotmail.com>
2015-02-28 16:43:51 +0100

    Removed xxxProperty from Group

--- src/main/java/no/ntnu/stud/model/Group.java
+++ src/main/java/no/ntnu/stud/model/Group.java
@@ -10,45 +10,32 @@ import java.util.ArrayList;
  */
 public class Group extends ArrayList<User> {
 
-    private IntegerProperty groupID;
-    private StringProperty name;
+    private int groupID;
+    private String name;
     private ArrayList<User> group;
 
     public Group() {
         super();
     }
 
-    public Group(IntegerProperty groupID, StringProperty name) {
+    public Group(int groupID, String name) {
         this.groupID = groupID;
         this.name = name;
     }
 
-    public Group(int groupID, String name) {
-        this.groupID.set(groupID);
-        this.name.set(name);
-    }
-
     public int getGroupID() {
-        return groupID.get();
-    }
-
-    public IntegerProperty groupIDProperty() {
         return groupID;
     }
 
     public void setGroupID(int groupID) {
-        this.groupID.set(groupID);
+        this.groupID=groupID;
     }
 
     public String getName() {
-        return name.get();
-    }
-
-    public StringProperty nameProperty() {
         return name;
     }
 
     public void setName(String name) {
-        this.name.set(name);
+        this.name=name;
     }
 }
