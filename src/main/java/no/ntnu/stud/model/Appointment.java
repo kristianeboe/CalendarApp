930c39094efe37b55479de7205a1e275172a6a96
kristandre <kristoffer.r.andresen@gmail.com>
2015-03-02 11:32:08 +0100

    Added validation to Appointment

--- src/main/java/no/ntnu/stud/model/Appointment.java
+++ src/main/java/no/ntnu/stud/model/Appointment.java
@@ -25,27 +25,45 @@ public class Appointment {
         setAttending(attending);
 
         // appointmentID is something we get from database after appointment is created, and should be used to instantiate Appointments.
-        this.appointmentID=appointmentID;
+        setAppointmentID(appointmentID);
 
         // Do we need any validation for this?
         // We could implement a generic textInputValidator, which validates that the input is not empty, not longer than x chars +++
-        this.title=title;
+        setTitle(title);
 
         // Get from DB. Map to User-object?
-        this.ownerID=ownerID;
+        setOwnerID(ownerID);
 
         // Do we need any validation for this?
-        this.location =location;
+        setLocation(location);
 
         // Get from DB. Map to Room-object?
-        this.roomID=roomID;
+        setRoomID(roomID);
 
         // Do we need any validation for this?
-        this.description = description;
+        setDescription(description);
 
         // Should alarmTime be at appointment start, 15 minutes before, 60 minutes before…
         //User defined?
-        this.alarmTime = alarmTime;
+        setAlarmTime(alarmTime);
+    }
+
+    public void setAppointmentID(int appointmentID){
+        if(appointmentID < 0){
+            throw new IllegalArgumentException("appointmentID cannot be a negative number");
+        }else if(appointmentID == 0){
+            throw new IllegalArgumentException("appointmentId cannot be 0");
+        }
+        this.appointmentID = appointmentID;
+    }
+
+    public void setOwnerID(int ownerID){
+        if(ownerID < 0){
+            throw new IllegalArgumentException("ownerID cannot be a negative number");
+        }else if(ownerID == 0){
+            throw new IllegalArgumentException("ownerID cannot be 0");
+        }
+        this.ownerID = ownerID;
     }
 
     public void setAttending(int attending) {
@@ -68,18 +86,40 @@ public class Appointment {
         if (title.isEmpty()){
             throw new IllegalArgumentException("Title must be defined");
         } else if (title.length() > 40){
-            throw new IllegalArgumentException("Title can't be this long, try shortening it");
+            throw new IllegalArgumentException("Title cannot be longer than 40 characters");
         }
         this.title = title;
     }
 
     public void setLocation(String location) {
-        //if () Enten hente liste over romnavn fra databasen, men det blir kanskje dyrt? Evt Lage en arraylist eller et set et sted hvor man henter ned romlisten når appen initialiseres
+        if(location.isEmpty()){
+            throw new IllegalArgumentException("location cannot be empty");
+        }else if(location.length()>100){
+            throw new IllegalArgumentException("location cannot be longer than 100 characters");
+        }
         this.location = location;
     }
 
     public void setDescription(String description) {
+        if(description.isEmpty()){
+            throw new IllegalArgumentException("description cannot be empty");
+        }else if(description.length()>250){
+            throw new IllegalArgumentException("description cannot be longer than 250 characters");
+        }
         this.description = description;
+    }
+
+    public void setRoomID(int roomID){
+        if(roomID < 0){
+            throw new IllegalArgumentException("roomID cannot be a negative number");
+        }else if(roomID == 0){
+            throw new IllegalArgumentException("roomID cannot be 0");
+        }
+        this.roomID = roomID;
+    }
+
+    public void setAlarmTime(LocalDateTime alarmTime){
+        this.alarmTime = alarmTime;
     }
 
     public LocalTime getStart() {
