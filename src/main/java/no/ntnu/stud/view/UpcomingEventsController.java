af5b0e6daa2eddbdf4f6ced60bbb76cf0c4507cb
Kristian Elset Boe <kristian.e.boe@gmail.com>
2015-03-02 14:56:05 +0100

    Connect all the fxml! + refactoret controllerfilernavn til aa samsvare med .fxml filene

---
+++ src/main/java/no/ntnu/stud/view/UpcomingEventsController.java
@@ -0,0 +1,19 @@
+package no.ntnu.stud.view;
+
+import no.ntnu.stud.MainApp;
+
+/**
+ * Created by Kristian on 02/03/15.
+ */
+public class UpcomingEventsController {
+
+    private MainApp mainApp;
+
+    public UpcomingEventsController(){
+
+    }
+
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
+}
---
+++ src/main/java/no/ntnu/stud/view/newAppointmentController.java
@@ -0,0 +1,19 @@
+package no.ntnu.stud.view;
+
+
+import no.ntnu.stud.MainApp;
+
+/**
+ * Created by Kristian on 25/02/15.
+ */
+public class NewAppointmentController {
+    private MainApp mainApp;
+
+    public NewAppointmentController() {
+
+    }
+
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
+}
