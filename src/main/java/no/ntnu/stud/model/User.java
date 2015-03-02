d347929dfb89ee5b4a59378975bda04d66616aa2
kristandre <kristoffer.r.andresen@gmail.com>
2015-03-02 11:20:25 +0100

    Added validation to User

--- src/main/java/no/ntnu/stud/model/User.java
+++ src/main/java/no/ntnu/stud/model/User.java
@@ -5,6 +5,9 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by adrianh on 23.02.15.
  */
@@ -31,6 +34,11 @@ public class User {
 
 
     public void setUserID(int userID) {
+        if(userID < 0){
+            throw new IllegalArgumentException("userID cannot be a negative number");
+        }else if(userID == 0){
+            throw new IllegalArgumentException("userID cannot be 0");
+        }
         this.userID=userID;
     }
 
@@ -40,6 +48,11 @@ public class User {
 
 
     public void setLastName(String lastName) {
+        if(lastName.isEmpty()){
+            throw new IllegalArgumentException("lastName cannot be empty");
+        }else if(lastName.length()>45){
+            throw new IllegalArgumentException("lastName cannot be longer than 45 characters");
+        }
         this.lastName=lastName;
     }
 
@@ -49,6 +62,11 @@ public class User {
 
 
     public void setMiddleName(String middleName) {
+        if(middleName.isEmpty()){
+            throw new IllegalArgumentException("middleName cannot be empty");
+        }else if(middleName.length()>45){
+            throw new IllegalArgumentException("middleName cannot be longer than 45 characters");
+        }
         this.middleName=middleName;
     }
 
@@ -58,6 +76,11 @@ public class User {
 
 
     public void setGivenName(String givenName) {
+        if(givenName.isEmpty()){
+            throw new IllegalArgumentException("givenName cannot be empty");
+        }else if(givenName.length()>45){
+            throw new IllegalArgumentException("givenName cannot be longer than 45 characters");
+        }
         this.givenName=givenName;
     }
 
@@ -67,6 +90,13 @@ public class User {
 
 
     public void setEmail(String email) {
+        final Pattern VALID_EMAIL_ADDRESS_REGEX =
+                Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+        if(!matcher.matches()){
+            throw new IllegalArgumentException("Invalid email-address");
+        }
+
         this.email=email;
     }
 }
