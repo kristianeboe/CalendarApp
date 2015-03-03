72cf0315786b91dc8aad87b7e66de4d7749aab90
kristoffer <krisrand@stud.ntnu.no>
2015-03-03 14:50:38 +0100

    Label fxml

--- src/main/java/no/ntnu/stud/view/CalendarViewController.java
+++ src/main/java/no/ntnu/stud/view/CalendarViewController.java
@@ -12,12 +12,12 @@ public class CalendarViewController {
     private MainApp mainApp;
 
     @FXML
-    private Text right_arrow;
+    private Label right_arrow, left_arrow;
 
     @FXML
     private Label d00,d01,d02,d03,d04,d05,d06,d10,d11,d12,d13,d14,d15,d16,d20,d21,d22,d23,d24,d25,d26,d30,d31,d32,d33,d34,d35,d36,d40,d41,d42,d43,d44,d45,d46,d50,d51;
-    @FXML
-    private Text left_arrow;
+
+
 
 
     //dele opp i dayViewController og Week-og/eller monthViewController?
