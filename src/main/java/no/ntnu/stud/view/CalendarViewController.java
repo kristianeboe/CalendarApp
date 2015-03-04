e4f484881304dad1c92a959b8df8c58b0cf0b80c
kristoffer <krisrand@stud.ntnu.no>
2015-03-04 10:35:09 +0100

    Created next/prev month features

--- src/main/java/no/ntnu/stud/view/CalendarViewController.java
+++ src/main/java/no/ntnu/stud/view/CalendarViewController.java
@@ -34,18 +34,32 @@ public class CalendarViewController {
     public void initialize(){
 
         calendar = Calendar.getInstance();
-        lblNextYear.setText("" + (calendar.get(Calendar.YEAR) + 1));
-        lblPrevYear.setText(""+(calendar.get(Calendar.YEAR)-1));
-        lblCurrentMonth.setText(TimeConverter.monthToString(calendar.get(Calendar.MONTH)) + " " + calendar.get(Calendar.YEAR));
+        calendar.set(2015,3,1);
         calendar.setFirstDayOfWeek(Calendar.MONDAY);
         renderCalendar(calendar);
 
     }
 
     public void renderCalendar(Calendar calendar){
-        dates.addAll(Arrays.asList(d00,d01,d02,d03,d04,d05,d06,d10,d11,d12,d13,d14,d15,d16,d20,d21,d22,d23,d24,d25,d26,d30,d31,d32,d33,d34,d35,d36,d40,d41,d42,d43,d44,d45,d46,d50,d51));
+        dates.clear();
+        dates.addAll(Arrays.asList(d00, d01, d02, d03, d04, d05, d06, d10, d11, d12, d13, d14, d15, d16, d20, d21, d22, d23, d24, d25, d26, d30, d31, d32, d33, d34, d35, d36, d40, d41, d42, d43, d44, d45, d46, d50, d51));
+
+        //Set Year and month labels
+        lblNextYear.setText("" + (calendar.get(Calendar.YEAR) + 1));
+        lblPrevYear.setText(""+(calendar.get(Calendar.YEAR)-1));
+        lblCurrentMonth.setText(TimeConverter.monthToString(calendar.get(Calendar.MONTH)) + " " + calendar.get(Calendar.YEAR));
+
+        // Get first day of month and total days in month
         int startOfMonth = getFirstDayOfMonth(calendar);
         int totalDays = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        //Fill with "-" before first day of month
+        for(int i = 0; i < startOfMonth;i++){
+            dates.get(i).setText("-");
+            dates.get(i).getStyleClass().remove("dates");
+        }
+
+        //Render calendar dates
         int j = 1;
         for(int i = startOfMonth; i < dates.size(); i++){
             if(j>totalDays){
@@ -75,6 +89,17 @@ public class CalendarViewController {
         return -1;
     }
 
+    @FXML
+    void nextMonth(){
+        calendar.add(Calendar.MONTH,1);
+        renderCalendar(calendar);
+    }
+
+    @FXML
+    void previousMonth(){
+        calendar.add(Calendar.MONTH,-1);
+        renderCalendar(calendar);
+    }
 
 
 
