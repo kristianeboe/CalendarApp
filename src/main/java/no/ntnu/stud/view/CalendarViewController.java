cab126698a87ef6f9a70f496928cc5a1964ece18
kristoffer <krisrand@stud.ntnu.no>
2015-03-04 08:43:12 +0100

    Created renderCalendar(not working)

--- src/main/java/no/ntnu/stud/view/CalendarViewController.java
+++ src/main/java/no/ntnu/stud/view/CalendarViewController.java
@@ -5,10 +5,8 @@ import javafx.scene.control.Label;
 import no.ntnu.stud.MainApp;
 import no.ntnu.stud.util.TimeConverter;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by Kristian on 25/02/15.
@@ -23,7 +21,7 @@ public class CalendarViewController {
     @FXML
     private Label d00,d01,d02,d03,d04,d05,d06,d10,d11,d12,d13,d14,d15,d16,d20,d21,d22,d23,d24,d25,d26,d30,d31,d32,d33,d34,d35,d36,d40,d41,d42,d43,d44,d45,d46,d50,d51;
 
-    private ArrayList<Label> dates = new ArrayList<Label>(Arrays.asList(d00,d01,d02,d03,d04,d05,d06,d10,d11,d12,d13,d14,d15,d16,d20,d21,d22,d23,d24,d25,d26,d30,d31,d32,d33,d34,d35,d36,d40,d41,d42,d43,d44,d45,d46,d50,d51));
+    private ArrayList<Label> dates = new ArrayList<>();
 
 
 
@@ -32,11 +30,43 @@ public class CalendarViewController {
     public CalendarViewController() {
 
     }
-    @FXML
-    public void initialize(){
-        calendar = new GregorianCalendar();
-        lblCurrentMonth.setText(TimeConverter.monthToString(calendar.MONTH)+" "+calendar.get(Calendar.YEAR));
 
+    public void initialize(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        calendar = Calendar.getInstance();
+        calendar.set(2015,1,1);
+        System.out.println(dateFormat.format(calendar.getTime()));
+        System.out.println(calendar.get(Calendar.MONTH));
+        lblCurrentMonth.setText(TimeConverter.monthToString(calendar.get(Calendar.MONTH)) + " " + calendar.get(Calendar.YEAR));
+        calendar.setFirstDayOfWeek(Calendar.MONDAY);
+        renderCalendar(calendar);
+
+    }
+
+    public void renderCalendar(Calendar calendar){
+        dates.addAll(Arrays.asList(d00,d01,d02,d03,d04,d05,d06,d10,d11,d12,d13,d14,d15,d16,d20,d21,d22,d23,d24,d25,d26,d30,d31,d32,d33,d34,d35,d36,d40,d41,d42,d43,d44,d45,d46,d50,d51));
+        int startOfMonth = calendar.DAY_OF_WEEK-1;
+
+        System.out.println(calendar.DAY_OF_WEEK+"som = "+startOfMonth + "motnh : "+ calendar.get(Calendar.MONTH) + "year: " + calendar.get(Calendar.YEAR));
+        int totalDays = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        System.out.println("flsmakfma: "+calendar.getFirstDayOfWeek());
+        System.out.println(totalDays);
+        //System.out.println(dates.get(1).getText());
+        int j = 1;
+        for(int i = startOfMonth; i < dates.size(); i++){
+            if(j>totalDays){
+                dates.get(i).setText("-");
+                dates.get(i).getStyleClass().remove("dates");
+            }else{
+                dates.get(i).setText((""+(j++)));
+                dates.get(i).getStyleClass().add("dates");
+            }
+        }
+    }
+
+    private Date getFirstDateOfCurrentMonth(Calendar cal) {
+        cal.set(Calendar.DAY_OF_MONTH, Calendar.getInstance().getActualMinimum(Calendar.DAY_OF_MONTH));
+        return cal.getTime();
     }
 
 
