fef3eeebfb1b61159b725fcfc261a14f4f313c93
kristoffer <krisrand@stud.ntnu.no>
2015-03-09 18:16:01 +0100

    push

--- src/main/java/no/ntnu/stud/view/UpcomingEventsController.java
+++ src/main/java/no/ntnu/stud/view/UpcomingEventsController.java
@@ -2,8 +2,14 @@ package no.ntnu.stud.view;
 
 import javafx.collections.ObservableArray;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import no.ntnu.stud.MainApp;
 import no.ntnu.stud.jdbc.GetData;
 import no.ntnu.stud.model.Appointment;
@@ -27,6 +33,9 @@ public class UpcomingEventsController {
     private Button btnNewAppointment, btnEditAppointment;
 
     @FXML ScrollPane scrollPane;
+
+    @FXML
+    AnchorPane ap;
 
     @FXML
     private GridPane upcomingEvents;
@@ -65,8 +74,51 @@ public class UpcomingEventsController {
                 }
             }
         }*/
-
+        int counter = 0;
         GridPane gp = new GridPane();
+        gp.setPrefHeight(65);
+        gp.setPrefWidth(175);
+        gp.setMinWidth(175);
+        gp.setLayoutX(0);
+        gp.getColumnConstraints().add(new ColumnConstraints(100));
+        gp.getColumnConstraints().add(new ColumnConstraints(100));
+        gp.getRowConstraints().add(new RowConstraints(49));
+        gp.getRowConstraints().add(new RowConstraints(15));
+        gp.setLayoutY(75*counter);
+
+        Button acceptBtn = new Button();
+        acceptBtn.setText("✓");
+        acceptBtn.maxHeight(25);
+        acceptBtn.maxWidth(25);
+
+        Button declineBtn = new Button();
+        declineBtn.setText("x");
+        declineBtn.maxHeight(25);
+        declineBtn.maxWidth(25);
+
+        GridPane.setValignment(declineBtn, VPos.BOTTOM);
+        GridPane.setHalignment(declineBtn, HPos.LEFT);
+        GridPane.setValignment(acceptBtn, VPos.BOTTOM);
+        GridPane.setConstraints(acceptBtn, 1, 0);
+        GridPane.setConstraints(declineBtn, 1,0);
+
+
+        Separator separator = new Separator();
+        separator.setPrefWidth(30);
+
+        GridPane.setConstraints(separator, 0,1,2,1);
+
+        Label label = new Label();
+        label.getStyleClass().add("meetings");
+        label.setText("Meeting");
+        label.maxWidth(140);
+
+        GridPane.setConstraints(label, 0,0,2,1);
+
+        gp.getChildren().addAll(acceptBtn, declineBtn, label, separator);
+
+        ap.getChildren().add(gp);
+
     }
 
     @FXML
