3872f8641be318ff54ba9fa7e9b47564380ec9b5
kristoffer <krisrand@stud.ntnu.no>
2015-03-19 09:23:03 +0100

    Fixed invite groups to appointment.

--- src/main/java/no/ntnu/stud/view/NewAppointmentController.java
+++ src/main/java/no/ntnu/stud/view/NewAppointmentController.java
@@ -356,8 +356,13 @@ public class NewAppointmentController {
                 inpInvite.clear();
             }else if(!searchResultsGroups.isEmpty()){
                 Group grp = searchResultsGroups.get(index);
-                invitedGroups.add(grp);
-                addGroupToInvitedBox(grp);
+                ArrayList<User> usersInGro = GetData.getUsersInGroup(grp.getGroupID());
+                for(User usr:usersInGro){
+                    invitedUsers.add(usr);
+                    addUserToInvitedBox(usr);
+                }
+                //invitedGroups.add(grp);
+                //addGroupToInvitedBox(grp);
                 inpInvite.clear();
             }
             //invitedUsersList.getItems().clear();
